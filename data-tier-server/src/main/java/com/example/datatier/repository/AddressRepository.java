e1b6643274245612b361e27621f78a8dd59deba2
MirkoPrevenzano <143605485+MirkoPrevenzano@users.noreply.github.com>
2024-11-30 22:59:12 +0100

    Delete data-tier-server directory

--- data-tier-server/src/main/java/com/example/datatier/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.datatier.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.datatier.model.Address;
-
-public interface AddressRepository extends JpaRepository<Address,Long>{
-    
-}
--- data-tier-server/src/main/java/com/example/datatier/repository/PropertySellRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.datatier.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.datatier.model.PropertySell;
-
-public interface PropertySellRepository extends JpaRepository<PropertySell,Long>{
-
-}
