96074aed29582dd593910c04e65fa24c9cf1bbcf
Ciro Pizza <129529624+cyropi@users.noreply.github.com>
2024-12-09 10:42:37 +0100

    Update Address.java

--- Progetto/data-tier-server/src/main/java/com/example/datatier/model/Address.java
+++ Progetto/data-tier-server/src/main/java/com/example/datatier/model/Address.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-jlhvjlhvkjlhvblhv
 
 @Entity
 @Table(name="address")
