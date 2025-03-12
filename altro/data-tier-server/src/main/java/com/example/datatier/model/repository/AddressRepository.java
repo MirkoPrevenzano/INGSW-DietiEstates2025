afd583f68494ec55ba9bad4cc0a21a66a9ead8a9
MirkoPrevenzano <m.prevenzano@studenti.unina.it>
2025-03-12 09:33:47 +0100

    eliminato server obsoleto

--- altro/data-tier-server/src/main/java/com/example/datatier/model/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.datatier.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.datatier.model.Address;
-
-public interface AddressRepository extends JpaRepository<Address,Long>{
-    
-}
--- altro/data-tier-server/src/main/java/com/example/datatier/model/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.datatier.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.datatier.model.Photo;
-
-public interface PhotoRepository extends JpaRepository<Photo,Long>{}
