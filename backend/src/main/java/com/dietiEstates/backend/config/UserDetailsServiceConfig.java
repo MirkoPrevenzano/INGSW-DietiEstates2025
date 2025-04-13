86715e29a42af9403de095fc1628292316652c9b
cyropi <ciropizza2002@gmail.com>
2025-04-13 18:07:21 +0200

    piccole modifiche UserDetailsServiceConfig, rimozione backend.zip

--- backend/src/main/java/com/dietiEstates/backend/config/UserDetailsServiceConfig.java
+++ backend/src/main/java/com/dietiEstates/backend/config/UserDetailsServiceConfig.java
@@ -38,7 +38,7 @@ public class UserDetailsServiceConfig
     @Autowired
     private HttpServletRequest httpServletRequest;
 
-    
+
 
     @Bean
     public UserDetailsService userDetailsService()
@@ -55,8 +55,8 @@ public class UserDetailsServiceConfig
                         Optional<Customer> customer = customerRepository.findByUsername(username);
                         if(customer.isEmpty())
                         {
-                            log.error("Customer not found in database");
-                            throw new UsernameNotFoundException("Customer not found in database");
+                            log.error("Customer not found in database!");
+                            throw new UsernameNotFoundException("Customer not found in database!");
                         }
                         else
                         {
@@ -71,12 +71,12 @@ public class UserDetailsServiceConfig
                         Optional<RealEstateAgent> realEstateAgent = realEstateAgentRepository.findByUsername(username);
                         if(realEstateAgent.isEmpty())
                         {
-                            log.error("Real Estate Agent not found in database");
-                            throw new UsernameNotFoundException("Real Estate Agent not found in database");
+                            log.error("Agent not found in database!");
+                            throw new UsernameNotFoundException("Agent not found in database!");
                         }
                         else
                         {
-                            log.info("Real Estate Agent found in database: {}", username);
+                            log.info("Agent found in database: {}", username);
                             realEstateAgent.get().setRole(Role.ROLE_AGENT);
                             return realEstateAgent.get();
                         }                        
@@ -87,23 +87,23 @@ public class UserDetailsServiceConfig
                         Optional<Administrator> administrator = administratorRepository.findByUsername(username);
                         if(administrator.isEmpty())
                         {
-                            log.error("Administrator not found in database");
-                            throw new UsernameNotFoundException("Administrator not found in database");
+                            log.error("Administrator not found in database!");
+                            throw new UsernameNotFoundException("Administrator not found in database!");
                         }
                         else if(passwordEncoder.matches("default", administrator.get().getPassword()))
                         {
-                            log.info("Administrator found in database: {}", username);
+                            log.info("Administrator (NOT AUTHORIZED) found in database: {}", username);
                             administrator.get().setRole(Role.ROLE_UNAUTHORIZED);
                             return administrator.get();                           
                         }
                         else if(administrator.get().getUserId() == 1)
                         {
-                            log.info("Administrator found in database: {}", username);
+                            log.info("Administrator (ADMIN) found in database: {}", username);
                             administrator.get().setRole(Role.ROLE_ADMIN);
                             return administrator.get();                           
                         }
                         
-                        log.info("Administrator found in database: {}", username);
+                        log.info("Administrator (COLLABORATOR) found in database: {}", username);
                         administrator.get().setRole(Role.ROLE_COLLABORATOR);
                         return administrator.get();                           
                     }
