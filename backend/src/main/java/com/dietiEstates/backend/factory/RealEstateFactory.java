8f6053193487810f928bb231b61fe390476882c1
cyropi <ciropizza2002@gmail.com>
2025-06-12 06:26:31 +0200

    tolti import inutili

--- backend/src/main/java/com/dietiEstates/backend/factory/RealEstateFactory.java
+++ backend/src/main/java/com/dietiEstates/backend/factory/RealEstateFactory.java
@@ -1,14 +1,11 @@
 
 package com.dietiEstates.backend.factory;
  
-import org.springframework.stereotype.Component;
 import com.dietiEstates.backend.dto.RealEstateCreationDTO;
 import com.dietiEstates.backend.dto.RealEstateForRentCreationDTO;
 import com.dietiEstates.backend.dto.RealEstateForSaleCreationDTO;
 import com.dietiEstates.backend.model.entity.RealEstate;
 import com.dietiEstates.backend.util.RealEstateMappingUtil;
-
-import lombok.RequiredArgsConstructor;
 
 
 
