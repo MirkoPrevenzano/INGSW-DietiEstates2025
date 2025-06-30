d06a45d3e0f2d8e4bb325bca507ffba69bcc2c02
cyropi <ciropizza2002@gmail.com>
2025-06-30 18:14:37 +0200

    inizio modifiche per la validazione dei DTO

--- backend/src/main/java/com/dietiEstates/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/dietiEstates/backend/exception/GlobalExceptionHandler.java
@@ -1,10 +1,16 @@
 
 package com.dietiEstates.backend.exception;
 
+import java.util.HashMap;
+
 import org.modelmapper.MappingException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.dietiEstates.backend.dto.response.ApiErrorResponse;
+
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -12,10 +18,27 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GlobalExceptionHandler 
 {
-    @ExceptionHandler(exception = MappingException.class)
+    @ExceptionHandler(MappingException.class)
     public ResponseEntity<?> handleError(MappingException e) 
     {
         return ResponseEntity.badRequest().header("Error", 
         "Problems while mapping! Probably the source object was different than the one expected!").body(null);
     }
-}
+
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e) 
+    {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> 
+            errors.put(error.getField(), error.getDefaultMessage()));
+        
+        ErrorResponse errorResponse = new ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            "Validation Failed",
+            errors
+        );
+        
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
