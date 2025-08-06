10c35a09f0d700d86b0bb10077f02138453ab9ff
cyropi <ciropizza2002@gmail.com>
2025-08-06 21:37:40 +0200

    aggiornato CustomerService

--- backend/src/main/java/com/dietiEstates/backend/controller/CustomerController.java
+++ backend/src/main/java/com/dietiEstates/backend/controller/CustomerController.java
@@ -3,14 +3,6 @@ package com.dietiEstates.backend.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import com.dietiEstates.backend.dto.request.RealEstateCreationDTO;
-import com.dietiEstates.backend.service.CustomerService;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,13 +12,4 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class CustomerController 
-{
-    private final CustomerService customerService;
-
-    
-    @GetMapping("{username}/view/{realEstateId}")
-    public ResponseEntity<RealEstateCreationDTO> viewRealEstate(@PathVariable("username") String username, @PathVariable("realEstateId") Long realEstateId, Authentication authentication) 
-    {
-        return ResponseEntity.ok().body(customerService.viewRealEstate(username, realEstateId,authentication));
-    }
-}
+{}
--- backend/src/main/java/com/dietiEstates/backend/service/CustomerService.java
+++ backend/src/main/java/com/dietiEstates/backend/service/CustomerService.java
@@ -1,44 +1,13 @@
 
 package com.dietiEstates.backend.service;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import com.dietiEstates.backend.dto.request.RealEstateCreationDTO;
-import com.dietiEstates.backend.dto.request.RealEstateForRentCreationDTO;
-import com.dietiEstates.backend.dto.request.RealEstateForSaleCreationDTO;
-import com.dietiEstates.backend.dto.request.support.AddressDTO;
-import com.dietiEstates.backend.dto.request.support.RealEstateBooleanFeaturesDTO;
-import com.dietiEstates.backend.dto.request.support.RealEstateLocationFeaturesDTO;
-import com.dietiEstates.backend.dto.request.support.RealEstateMainFeaturesDTO;
-import com.dietiEstates.backend.enums.EnergyClass;
-import com.dietiEstates.backend.enums.FurnitureCondition;
-import com.dietiEstates.backend.enums.NotaryDeedState;
-import com.dietiEstates.backend.enums.PropertyCondition;
-import com.dietiEstates.backend.model.embeddable.CustomerViewsRealEstateId;
-import com.dietiEstates.backend.model.embeddable.ExternalRealEstateFeatures;
-import com.dietiEstates.backend.model.embeddable.InternalRealEstateFeatures;
-import com.dietiEstates.backend.model.entity.Address;
-import com.dietiEstates.backend.model.entity.Agent;
 import com.dietiEstates.backend.model.entity.Customer;
-import com.dietiEstates.backend.model.entity.CustomerViewsRealEstate;
-import com.dietiEstates.backend.model.entity.RealEstate;
-import com.dietiEstates.backend.model.entity.RealEstateForRent;
-import com.dietiEstates.backend.model.entity.RealEstateForSale;
 import com.dietiEstates.backend.repository.CustomerRepository;
-import com.dietiEstates.backend.repository.RealEstateRepository;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -49,157 +18,10 @@ import java.util.UUID;
 @Slf4j
 public class CustomerService 
 {
-    private final RealEstateRepository realEstateRepository;
     private final CustomerRepository customerRepository;
-    private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
 
-
-
-    public RealEstateCreationDTO viewRealEstate(String username, Long realEstateId, Authentication authentication) 
-    {
-        log.info("\n\nENTRO NEL METODO VIEW REAL ESTATE:\n");
-
-        log.info("\n\nFIND BY USERNAME (CustomerRepo):\n");
-        Customer customer = customerRepository.findByUsername(username).get();
-
-        log.info("\n\nFIND BY ID (RealEstateRepo):\n");
-        RealEstate realEstate = realEstateRepository.findById(realEstateId).get();
-
-        log.info("\n\nGET AGENT FROM REAL ESTATE:\n");
-        Agent agent = realEstate.getAgent();
-
-/*         
-find user;
-find real estate;
-
-realestate.getAgent 
-modelmapper.map(agentinfo,agent)
-map from realestate to realestate creation
-create realestatecompleteDTO
-
-if(context == customer)
-        updating realestate viewsnumber
-        creazione custviewrealest
-        save custview
-
-return realestatecompleteDTO;
-
- */
-
-         // 2. Verifica se l'utente autenticato ha il ruolo CUSTOMER
-        if(authentication != null && authentication.isAuthenticated()) 
-        {
-            if(authentication.getPrincipal() instanceof Customer)
-                log.info("\n\nil mio principal è un customerrrr\n\n");
-
-                if(authentication.getPrincipal() instanceof Agent)
-                log.info("\n\nil mio principal è un agentttt\n\n");
-
-
-            UserDetails user = (UserDetails) authentication.getPrincipal();
-
-            log.info("\n\n\nSONO NELL'IF DEL CUSTOMERR");
-        }
-           
-        RealEstateCreationDTO realEstateCreationDTO = realEstateCreationDTOMapper(realEstate);
-                
-
-        
-                long newViewsNumber = realEstate.getRealEstateStats().getViewsNumber() + 1;
-                realEstate.getRealEstateStats().setViewsNumber(newViewsNumber);
-        
-                CustomerViewsRealEstate customerViewsRealEstate = new CustomerViewsRealEstate(
-                                                                        new CustomerViewsRealEstateId(customer.getUserId(), realEstate.getRealEstateId()), 
-                                                                        LocalDateTime.now(), 
-                                                                        customer,
-                                                                        realEstate);
-        
-                log.info("\n\nGET CUSTOMERVIEWSREALESTATE:\n");
-/*                 List<CustomerViewsRealEstate> customerViewsRealEstateList = customer.getCustomerViewsRealEstates();
-        
-                if(customerViewsRealEstateList.size() > 0)
-                {
-                    for(CustomerViewsRealEstate customerViewsRealEstate2 : customerViewsRealEstateList)
-                    {
-                        if(customerViewsRealEstate.getCustomerViewsRealEstateId().equals(customerViewsRealEstate2.getCustomerViewsRealEstateId()))
-                        {
-                            log.info("\n\nMERGE (CustomerRepo):\n");
-                            customerViewsRealEstate2.setViewDate(LocalDateTime.now());
-                            customerRepository.save(customer);
-                            return realEstateCreationDTO;
-                        }  
-                    }
-                } */
-                
-                customer.addCustomerViewsRealEstate(customerViewsRealEstate);
-                log.info("\n\nSAVE (CustomerRepo):\n");
-                customerRepository.save(customer);
-        
-                return realEstateCreationDTO;
-            }
-        
-        
-        
-            private RealEstateCreationDTO realEstateCreationDTOMapper(RealEstate realEstate) 
-            {
-                String title = realEstate.getTitle();
-                String description = realEstate.getDescription();
-                Double price = realEstate.getPrice();
-                Double condoFee = realEstate.getCondoFee();
-                EnergyClass energyClass = realEstate.getEnergyClass();
-
-                Double size = realEstate.getInternalFeatures().getSize();
-                Integer roomsNumber = realEstate.getInternalFeatures().getRoomsNumber();
-                PropertyCondition propertyCondition = realEstate.getInternalFeatures().getPropertyCondition();
-                FurnitureCondition furnitureCondition = realEstate.getInternalFeatures().getFurnitureCondition();
-
-                Integer parkingSpacesNumber = realEstate.getExternalFeatures().getParkingSpacesNumber();
-                Integer floorNumber = realEstate.getExternalFeatures().getFloorNumber();
-
-                RealEstateMainFeaturesDTO realEstateMainFeaturesDTO = new RealEstateMainFeaturesDTO(title, description, price, condoFee, size, 
-                                                                   roomsNumber, parkingSpacesNumber, floorNumber, 
-                                                                   energyClass, propertyCondition, furnitureCondition);
-
-
-                RealEstateBooleanFeaturesDTO realEstateBooleanFeaturesDTO = new RealEstateBooleanFeaturesDTO();
-                realEstateBooleanFeaturesDTO.setAirConditioning(realEstate.getInternalFeatures().isAirConditioning());
-                realEstateBooleanFeaturesDTO.setHeating(realEstate.getInternalFeatures().isHeating());
-                realEstateBooleanFeaturesDTO.setBalcony(realEstate.getExternalFeatures().isBalcony());
-                realEstateBooleanFeaturesDTO.setElevator(realEstate.getExternalFeatures().isElevator());
-                realEstateBooleanFeaturesDTO.setConcierge(realEstate.getExternalFeatures().isConcierge());
-                realEstateBooleanFeaturesDTO.setGarage(realEstate.getExternalFeatures().isGarage());
-                realEstateBooleanFeaturesDTO.setGarden(realEstate.getExternalFeatures().isGarden());
-                realEstateBooleanFeaturesDTO.setSwimmingPool(realEstate.getExternalFeatures().isSwimmingPool());
-                realEstateBooleanFeaturesDTO.setTerrace(realEstate.getExternalFeatures().isTerrace());
-
-
-                RealEstateLocationFeaturesDTO realEstateLocationFeaturesDTO = new RealEstateLocationFeaturesDTO();
-                realEstateLocationFeaturesDTO.setNearPark(realEstate.getExternalFeatures().isNearPark());
-                realEstateLocationFeaturesDTO.setNearSchool(realEstate.getExternalFeatures().isNearSchool());
-                realEstateLocationFeaturesDTO.setNearPublicTransport(realEstate.getExternalFeatures().isNearPublicTransport());
-
-
-                AddressDTO addressDTO = modelMapper.map(realEstate.getAddress(), AddressDTO.class);
-
-
-                if(realEstate instanceof RealEstateForSale)
-                {
-                    RealEstateForSale realEstateForSale = (RealEstateForSale) realEstate;
-                    NotaryDeedState notaryDeedState = realEstateForSale.getNotaryDeedState();
-                    return new RealEstateForSaleCreationDTO(addressDTO, realEstateMainFeaturesDTO, realEstateBooleanFeaturesDTO, realEstateLocationFeaturesDTO, notaryDeedState);
-                }
-                else
-                {
-                    RealEstateForRent realEstateForSale = (RealEstateForRent) realEstate;
-                    Double securityDeposit = realEstateForSale.getSecurityDeposit();
-                    Integer contractYears = realEstateForSale.getContractYears();
-                    return new RealEstateForRentCreationDTO(addressDTO, realEstateMainFeaturesDTO, realEstateBooleanFeaturesDTO, realEstateLocationFeaturesDTO, securityDeposit, contractYears);
-                }
-            }
             
-            
-
     /* 
     *Vedo se esiste l'utente, altrimenti lo genero e restituisco al authService l'oggetto User 
     */ 
