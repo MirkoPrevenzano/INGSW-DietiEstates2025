57bf0d038bc811d13cf454869fdf2466393f4034
cyropi <ciropizza2002@gmail.com>
2025-08-07 01:18:13 +0200

    creato AgencyService

---
+++ backend/src/main/java/com/dietiEstates/backend/service/AgencyService.java
@@ -0,0 +1,76 @@
+
+package com.dietiEstates.backend.service;
+
+import org.modelmapper.MappingException;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dietiEstates.backend.dto.request.CollaboratorRegistrationDTO;
+import com.dietiEstates.backend.dto.request.AgentRegistrationDTO;
+import com.dietiEstates.backend.exception.EmailServiceException;
+import com.dietiEstates.backend.model.entity.Administrator;
+import com.dietiEstates.backend.model.entity.Agent;
+import com.dietiEstates.backend.repository.AdministratorRepository;
+import com.dietiEstates.backend.repository.AgentRepository;
+import com.dietiEstates.backend.service.mail.AgentWelcomeEmailService;
+import com.dietiEstates.backend.service.mail.CollaboratorWelcomeEmailService;
+import com.dietiEstates.backend.service.mock.MockingStatsService;
+import com.dietiEstates.backend.util.PasswordGeneratorUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AgencyService 
+{
+    private final AdministratorRepository administratorRepository;
+    private final AgentRepository agentRepository;
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final MockingStatsService mockingStatsService;
+    private final AgentWelcomeEmailService agentWelcomeEmailService;
+    private final CollaboratorWelcomeEmailService collaboratorWelcomeEmailService;
+    
+
+
+    @Transactional
+    public void createCollaborator(String username, CollaboratorRegistrationDTO collaboratorRegistrationDTO) throws UsernameNotFoundException, 
+                                                                                    IllegalArgumentException, MappingException
+    {
+        Administrator administrator = administratorRepository.findByUsername(username)
+                                                             .orElseThrow(() -> new UsernameNotFoundException(""));
+
+        if(administratorRepository.findByUsername(collaboratorRegistrationDTO.getUsername()).isPresent())
+        {
+            log.error("This username is already present!");
+            throw new IllegalArgumentException("This username is already present!");
+        }
+
+        Administrator collaborator = modelMapper.map(collaboratorRegistrationDTO, Administrator.class);
+        
+        String plainTextPassword = PasswordGeneratorUtil.generateRandomPassword();
+        String hashedPassword = passwordEncoder.encode(plainTextPassword);
+
+        collaborator.setPassword(hashedPassword);
+        
+        administrator.addCollaborator(collaborator);
+        // admin = administratorRepository.save(admin);
+
+        log.info("Collaborator was created successfully!");
+
+        try 
+        {
+            collaboratorWelcomeEmailService.sendWelcomeEmail(collaborator);
+        } 
+        catch (EmailServiceException e) 
+        {
+            log.warn(e.getMessage());
+        }
+    }
+}
