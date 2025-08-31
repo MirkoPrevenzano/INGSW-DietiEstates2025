7dcf7f8b17082bf40082d5451c4dd4090e3fcfb0
cyropi <ciropizza2002@gmail.com>
2025-08-31 06:11:32 +0200

    ulteriori modifiche per migliorare documentazione OpenAPI/Swagger: aggiunta documentazione per path /login

--- backend/src/main/java/com/dietiestates/backend/config/OpenApiConfig.java
+++ backend/src/main/java/com/dietiestates/backend/config/OpenApiConfig.java
@@ -5,8 +5,16 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
@@ -51,6 +59,41 @@ public class OpenApiConfig
                                                    .description("Operazioni sulle agencies"),
                                           new Tag().name("Real Estates")
                                                    .description("Operazioni sui real estates")))
-                            .security(List.of(new SecurityRequirement().addList("jwtBearerAuth")));
+                            .security(List.of(new SecurityRequirement().addList("jwtBearerAuth")))
+                            .path("/login", createLoginPath());
     }
+
+
+    private PathItem createLoginPath()
+    {
+         return new PathItem().post(new Operation().summary("Login utente")
+                                                   .description("Autenticazione tramite username e password. Utilizza form data (application/x-www-form-urlencoded).")
+                                                   .security(List.of())
+                                                   .requestBody(new RequestBody().required(true)
+                                                                                 .description("Credenziali di accesso")
+                                                                                 .content(new Content().addMediaType("application/x-www-form-urlencoded", new MediaType().schema(getLoginRequestSchema()))))
+                                                   .responses(new ApiResponses().addApiResponse("200", new ApiResponse().description("Login effettuato con successo - restituisce il token JWT")
+                                                                                                                        .content(new Content().addMediaType("application/json", new MediaType().schema(getJwtTokenResponseSchema()))))));
+
+    }
+
+    private Schema<?> getLoginRequestSchema()
+    {
+         return new Schema<>().type("object")
+                              .addProperty("username", new Schema<>().type("string")
+                                                                     .description("Email dell'utente")
+                                                                     .example("login@example.com"))
+                              .addProperty("password", new Schema<>().type("string")
+                                                                     .format("password")
+                                                                     .description("Password dell'utente")
+                                                                     .example("password1234@!"));
+    }
+
+   private Schema<?> getJwtTokenResponseSchema()
+   {
+         return new Schema<>().type("object")
+                              .addProperty("accessToken", new Schema<>().type("string")
+                                                                        .description("Token JWT da usare per l'autenticazione dell'utente.")
+                                                                        .example("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."));
+   }
 }
