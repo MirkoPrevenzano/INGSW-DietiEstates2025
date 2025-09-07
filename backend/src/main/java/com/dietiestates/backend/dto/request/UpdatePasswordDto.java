48fe34710c519a9d2b0427b0084f681581f6aadf
cyropi <ciropizza2002@gmail.com>
2025-09-07 14:02:22 +0200

    risolto bug: applicato il controllo della nuova password durante richiesta di modifica password

--- backend/src/main/java/com/dietiestates/backend/dto/request/UpdatePasswordDto.java
+++ backend/src/main/java/com/dietiestates/backend/dto/request/UpdatePasswordDto.java
@@ -3,6 +3,8 @@ package com.dietiestates.backend.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
+import com.dietiestates.backend.validator.PasswordValidator;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import lombok.Data;
@@ -21,6 +23,7 @@ public class UpdatePasswordDto
     private String oldPassword;
 
     @NotBlank
+    @PasswordValidator
     @Schema(description = "Nuova password dall'utente.", example = "NewP@ssw0rd456!")
     private String newPassword;
 }
